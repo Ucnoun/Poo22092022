5c13bc0b98ad26e884d85877bd30b526eb6e184e
ilai <eliestevane@gmail.com>
2022-05-26 17:31:34 -0500

    Clase 26/05/22

---
+++ ico.fes/src/ico/fes/IcoFes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author elies
+ */
+public class IcoFes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
